e9c11b1c15030c035adda6ddc216e1d39fa223f5
Lyukio <lyukio@hotmail.com>
2024-02-07 01:15:04 -0300

    removing export app from index

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -7,7 +7,7 @@ import { crossDomain } from "../utils/crossDomain"
 import bodyParser from "body-parser"
 import db from "./db"
 
-export const app = express()
+const app = express()
 
 const port = process.env.PORT ?? 4568
 
